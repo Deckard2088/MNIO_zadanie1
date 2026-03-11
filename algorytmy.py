f683cf797f963d60964528740203cf74b82d7873
Dawid <dawid.wachecki@onet.pl>
2026-03-11 09:30:28 +0100

    dziala :3

--- algorytmy.py
+++ algorytmy.py
@@ -2,11 +2,17 @@ from math import cos
 
 #DEFINICJE PRZYKŁADOWE FUNKCJI
 #wielomian obliczany metodą hornera
-def wielomian(x, tablica_wspolczynnikow, dlugosc_tablicy):
+def horner(x, tablica_wspolczynnikow, dlugosc_tablicy):
     #Czyli zamiast np. y = 4*x*x*x + 3*x + 5 podaje się funkcje wielomian z argumentami: x, [4,0,3,5], 4
     y = tablica_wspolczynnikow[0]
     for i in range(1,dlugosc_tablicy,1):
         y = tablica_wspolczynnikow[i] + x * y
+    return y
+
+#przykładowy wielomian
+def wielomian(x):
+    #y = 4x^3 -6x^2 + 9x
+    y = horner(x, [4,-6,9,0], 4)
     return y
 
 #skoro to taka prosta funkcja to może ją wywalić? i po prostu import math dać? jak tak to te złożone może też? albo zmienić na inne
@@ -22,7 +28,7 @@ def wykladnicza(x):
 
 def zlozWielomianTryg(x):
     #y = cos(7x^3 - 6x)
-    y = trygonometryczna(wielomian(x, [7,0,0,-6,0], 5))
+    y = trygonometryczna(horner(x, [7,0,0,-6,0], 5))
     return y
 
 def zlozTrygWykl(x):
@@ -32,14 +38,25 @@ def zlozTrygWykl(x):
 
 def zlozWielomianWykl(x):
     #y = 3.14 do potęgi (3x^5 + 9x^2 - 20)
-    y = wykladnicza(wielomian(x, [3,0,0,9,0,-20],6))
+    y = wykladnicza(horner(x, [3,0,0,9,0,-20],6))
     return y
 
 def zlozWszystko(x):
     #y = cos(3.14 do potęgi (6x^2 - 7))
-    y = trygonometryczna(wykladnicza(wielomian(x, [6,0,-7],3)))
+    y = trygonometryczna(wykladnicza(horner(x, [6,0,-7],3)))
     return y
 
 def bisekcja(f,a,b):
-
-    return 0
+    #ten warunek to może do maina przenieść, żeby było bardziej OPTYMALNIE
+    if (f(a)*f(b) >= 0):
+        print("Podano nieprawidłowy przedział, nie da się wykorzystać metody bisekcji")
+        return
+    x0 = (a+b)/2
+    if (f(x0) == 0):
+        print("Pierwiastek został znaleziony")
+    elif (f(x0)*f(b) < 0):
+        a = x0
+    elif (f(x0)*f(a) < 0):
+        b = x0
+    print(a,b,"\n")
+    return a,b
--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 #Dawid Wachecki 254890
 #Kacper Skoczylas 254864
 #Zadanie 1: metoda bisekcji, reguła falsi, wariant A
-import algorytmy
-
-def petla(warunek):
+import algorytmy as alg
+#na początku programu niech narysuje tę funkcje na przedziale i dopiero potem te algorytmy
+def petla(warunek, wyborFunkcji, a, b):
     if (warunek == 1):
         #spełnienie konkretnej dokładności
         dokladnosc = input("Podaj dokladnosc: ")
 
     elif (warunek == 2):
         #konkretna liczba iteracji
-        liczbaIter = input("Podaj liczbę iteracji: ")
-
+        liczbaIter = int(input("Podaj liczbę iteracji: "))
+        for i in range(liczbaIter):
+            a,b = alg.bisekcja(wybranaFunkcja(wyborFunkcji), a, b)
     else:
         print("\nPodano błędną wartość")
+
+def wybranaFunkcja(wyborFunkcji):
+    if wyborFunkcji == 1:
+        f = alg.wielomian
+    elif wyborFunkcji == 2:
+        f = alg.trygonometryczna
+    elif wyborFunkcji == 3:
+        f = alg.wykladnicza
+    elif wyborFunkcji == 4:
+        f = alg.zlozWielomianTryg
+    elif wyborFunkcji == 5:
+        f = alg.zlozTrygWykl
+    elif wyborFunkcji == 6:
+        f = alg.zlozWielomianWykl
+    elif wyborFunkcji == 7:
+        f = alg.zlozWszystko
+    return f
 
 def menu():
     print("================================================")
@@ -28,20 +46,20 @@ def menu():
     print("5. złozenie trygonometrycznej i wykładniczej")
     print("6. złozenie wielomianu i wykładniczej")
     print("7. złozenie wszystkich trzech\n")
-    wyborFunkcji = input("WYBRANA FUNKCJA: ")
+    wyborFunkcji = int(input("WYBRANA FUNKCJA: "))
 
     print("\n================================================")
     print("Miejsca zerowe będą poszukiwane w przedziale [a-b]")
-    a = input("PODAJ WARTOŚĆ 'a': ")
-    b = input("PODAJ WARTOŚĆ 'b': ")
+    a = float(input("PODAJ WARTOŚĆ 'a': "))
+    b = float(input("PODAJ WARTOŚĆ 'b': "))
 
     print("\n================================================")
     print("WYBIERZ KRYTERIUM ZATRZYMANIA ALGORYTMU")
     print("1. spełnienie warunku nałożonego na dokładność")
     print("2. osiągnięcie zadanej liczby iteracji")
-    wyboruWarunku = input("WYBRANY WARUNEK: ")
+    wyboruWarunku = int(input("WYBRANY WARUNEK: "))
 
-    print(algorytmy.wielomian(4, [5,0,-2,3],4))
+    petla(wyboruWarunku, wyborFunkcji, a, b)
 
 #jak to by zrobić najbardziej optymalnie?
 #osobna funkcja z instukcją warunkową co do kryterium zatrzymania algorytmu?
